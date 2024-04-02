a7e2b711d57318c65aebb5f8bc530ff882bc2129
Iago Silva | Grupo Moura <iago.silva@grupomoura.com>
2024-04-01 23:03:38 -0300

    first commit :tada:

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { UpperCasePipe, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [
+    UpperCasePipe,
+    FormsModule,
+    NgIf
+  ],
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css'
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit} from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Hero } from '../hero';
+import { FormsModule } from '@angular/forms';
+import { HeroService } from '../hero.service';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [
+    HeroDetailComponent,
+    FormsModule,
+    NgFor,
+  ],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+}
